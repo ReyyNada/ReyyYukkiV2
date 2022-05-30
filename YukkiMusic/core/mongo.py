7297ebd44b6ae0b89fa763e44e4710dc5c482df1
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-30 09:36:49 +0700

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -20,7 +20,7 @@ TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirst
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "No MONGO DB URL found.. Your Bot will work on Yukki's Database"
+        "Tidak ada URL DB MONGO yang ditemukan.. Bot Anda akan berfungsi di Database Yukki"
     )
     temp_client = Client(
         "Yukki",
