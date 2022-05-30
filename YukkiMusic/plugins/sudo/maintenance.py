53490616501b3acae558ce4186d0bbaadd5e4719
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-30 17:18:48 +0700

    Update maintenance.py

--- YukkiMusic/plugins/sudo/maintenance.py
+++ YukkiMusic/plugins/sudo/maintenance.py
@@ -38,7 +38,7 @@ async def maintenance(client, message: Message):
     if state == "enable":
         if await is_maintenance() is False:
             await message.reply_text(
-                "Maintenance mode is already enabled"
+                "Mode pemeliharaan sudah diaktifkan"
             )
         else:
             await maintenance_on()
@@ -49,7 +49,7 @@ async def maintenance(client, message: Message):
             await message.reply_text(_["maint_3"])
         else:
             await message.reply_text(
-                "Maintenance mode is already disabled"
+                "Mode pemeliharaan sudah dimatikan"
             )
     else:
         await message.reply_text(usage)
