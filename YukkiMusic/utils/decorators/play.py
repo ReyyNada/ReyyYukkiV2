74e1e6ba519ec9f0d685559f82f6ca2ef19ab184
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-30 09:52:30 +0700

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -27,12 +27,12 @@ def PlayWrapper(command):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "Bot sedang dalam pemeliharaan.  Harap tunggu beberapa saat..."
                 )
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
+                    "**Bot Musik Pribadi**\n\nHanya untuk obrolan resmi dari pemiliknya.  Minta pemilik saya untuk mengizinkan obrolan Anda terlebih dahulu."
                 )
                 return await app.leave_chat(message.chat.id)
         if await is_commanddelete_on(message.chat.id):
