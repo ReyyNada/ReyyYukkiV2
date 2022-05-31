dafc4f90acb96fb72caed31c78edfa51909d1f80
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-05-31 07:53:01 +0700

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -12,10 +12,10 @@ import asyncio
 
 from pyrogram import Client as c
 
-API_ID = input("\nEnter Your API_ID:\n > ")
-API_HASH = input("\nEnter Your API_HASH:\n > ")
+API_ID = input("\nKirim API_ID Anda:\n > ")
+API_HASH = input("\nKirim API_HASH Anda:\n > ")
 
-print("\n\n Enter Phone number when asked.\n\n")
+print("\n\n Masukkan nomor telepon saat ditanya.\n\n")
 
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
@@ -23,7 +23,7 @@ i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 async def main():
     await i.start()
     ss = await i.export_session_string()
-    print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+    print("\nINI ADALAH SESI STRING ANDA, SALIN, JANGAN BAGIKAN!!\n")
     print(f"\n{ss}\n")
 
 
