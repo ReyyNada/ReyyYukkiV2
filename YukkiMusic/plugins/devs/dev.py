bb53808da714f957002d5840d6410d97503c77dd
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-09 20:23:36 +0700

    Update dev.py

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -43,7 +43,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 
 @app.on_message(
-    filters.command("eval")
+    filters.command("ynt")
     & SUDOERS
     & ~filters.forwarded
     & ~filters.via_bot
