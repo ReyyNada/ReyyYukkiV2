8a5705f018fd9ec843b2b9908e072a85e52153c0
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-18 03:02:10 +0700

    .

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -41,7 +41,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
         ],
-        ]
+        [
             InlineKeyboardButton(
                 text=_["S_B_9"], url=f"@BdrlBukan",
             ),
