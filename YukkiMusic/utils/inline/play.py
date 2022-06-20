a0bb7fd4ebccd65dbc396552beae95a82ed6e875
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-20 18:37:39 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -64,8 +64,7 @@ def stream_markup(_, videoid):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
+                text=_["S_B_9"], url=f"https://t.me/BdrlBukan",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"], switch_inline_query_current_chat=""
