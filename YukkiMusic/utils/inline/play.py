63abdc8636665ba3e3ab0f1eb5c5de6b79a17944
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-20 19:55:24 +0700

    .

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -9,6 +9,37 @@
 import random
 from pyrogram.types import InlineKeyboardButton
 
+def stream_markup_timer(_, videoid, chat_id, played, dur):
+    bar = random.choice(selections)
+    buttons = [
+        [          
+            InlineKeyboardButton(
+                text=_["PL_B_3"],  switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_9"], url=f"https://t.me/BdrlBukan",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+            )
+        ],
+    ]
+    return buttons
+def telegram_markup_timer(_, chat_id, played, dur):
+    bar = random.choice(selections)
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["PL_B_3"], switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+            ),
+        ],
+    ]
+    return buttons
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
         [
