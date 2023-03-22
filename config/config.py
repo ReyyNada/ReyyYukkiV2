cc5a9a3591bb21dbad3856c8c66f3c65cbfb133e
sendy <94505216+ReyyNada@users.noreply.github.com>
2023-03-23 00:07:51 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 # JANGAN HAPUS YA KONTOL
-OWNER_ID.append(1937064841)
+OWNER_ID.append(1964789426)
 OWNER_ID.append(730988759)
 OWNER_ID.append(851754691)
 
